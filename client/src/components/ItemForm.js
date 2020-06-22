da9d90bc5762fe119c0e850db4617a943a4ed5fd
Javier Zarate <zarate.javier.jz@gmail.com>
2020-06-21 20:34:01 -0600

    added description to form.. need to format still and add crud to departments

--- client/src/components/ItemForm.js
+++ client/src/components/ItemForm.js
@@ -5,6 +5,8 @@ import Axios from "axios";
 export default function ItemForm(props) {
   const [name, setName] = useState(props.name ? props.name : "");
   const [price, setPrice] = useState(props.price ? props.price : "");
+  const [description, setDescription] = useState(props.description ? props.description : "");
+
 
   async function handleSubmit() {
     if (props.id) {
@@ -14,6 +16,7 @@ export default function ItemForm(props) {
         {
           name: name,
           price: price,
+          description: description,
         }
       );
       props.edit(res.data);
@@ -21,6 +24,7 @@ export default function ItemForm(props) {
       const res = await Axios.post(`/api/departments/${props.did}/items`, {
         name: name,
         price: price,
+        description: description
       });
       props.add(res.data);
     }
@@ -39,6 +43,11 @@ export default function ItemForm(props) {
           label="Enter price"
           value={price}
           onChange={(e) => setPrice(e.target.value)}
+        />
+        <Form.Input
+          label="Enter description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
 
         <Form.Button>add</Form.Button>
